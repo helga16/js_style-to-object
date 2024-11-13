c244a134bb6fa511856463b59e226c914a6d15cd
kaplunova <melamori1605@gmail.com>
2024-11-13 14:41:42 +0200

    added function

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,16 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const arrayWithStyles = complexStylesString.split(';')
+    .map((el) => el.trim());
+  const resultObject = {};
+
+  for (const el of arrayWithStyles) {
+    const pair = el.split(':').map((el) => el.trim());
+    resultObject[pair[0]] = pair[1];
+  }
+
+  return resultObject;
 }
 
 module.exports = convertToObject;
